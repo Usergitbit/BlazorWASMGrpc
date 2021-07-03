12c376d2755b80d6983f7ed5662efd769465af77
Usergitbit <32998206+Usergitbit@users.noreply.github.com>
2021-07-03 22:32:06 +0300

    Add project files.

---
+++ BlazorWASMGrpc/Server/Grpc/WeatherGrpc.cs
@@ -0,0 +1,29 @@
+﻿using BlazorWASMGrpc.Shared;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWASMGrpc.Server.Grpc
+{
+    public class WeatherGrpc : WeatherProto.WeatherProtoBase
+    {
+        public override Task<GetReply> Get(GetRequest request, ServerCallContext context)
+        {
+            var reply = new GetReply
+            {
+                WeatherForecast = new WeatherForecast
+                {
+                    Date = Timestamp.FromDateTime(DateTime.UtcNow),
+                    Summary = "summary",
+                    TemperatureC = 35,
+                    TemperatureF = 100
+                }
+            };
+
+            return Task.FromResult(reply);
+        }
+    }
+}
